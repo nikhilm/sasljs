5c2d45af2b31303f03d8125f428be126b0967b61
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-04-10 10:48:03 +0530

    Modified example to test property get/set.
    TODO: introduce the string constants into javascript

--- example.js
+++ example.js
@@ -1,13 +1,14 @@
 var sasl = require('./sasl');
 var sys = require('sys');
 
-sasl_conn = sasl.createServerSession();
-sys.debug( sys.inspect( sasl_conn.mechanisms ) );
-sys.debug( sys.inspect( sasl ) );
+sasl_conn = sasl.createServerSession("realm", function(prop) {
+        if(prop == sasl.GSASL_REALM)
+            return "localhost";
+});
+sasl_conn.start("DIGEST-MD5");
+sys.debug( sys.inspect( sasl_conn ) );
+sys.debug( sasl_conn.property("realm") );
+sasl_conn.setProperty("realm", "localhost");
+sys.debug( sasl_conn.property("realm") );
+//sys.debug( sys.inspect( sasl ) );
 
-sys.debug("---------- START --------- ");
-var res = sasl_conn.start( "DIGEST-MD5" );
-sys.debug(sys.inspect(res));
-
-res = sasl_conn.step("");
-sys.debug(sys.inspect(res));
