d2e3910ee1ccd0e01ac50c560637d7c8ff7f5783
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-04-15 14:45:51 +0530

    Replaced sys.inherits with Object.create

--- sasl.js
+++ sasl.js
@@ -1,7 +1,7 @@
 var saslc = require('./lib/binding_sasl');
 var sys = require('sys');
 
-process.mixin( exports, saslc );
+exports.prototype = Object.create( saslc );
 exports.createServerSession = function(realm, callback) {
     var serv = new saslc.ServerSession( realm, callback );
     serv.mechanisms = serv._mechanisms().split(' ');
